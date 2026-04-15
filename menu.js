e323ac0426fd404f9cd575d112c110885227d451
kamalmarshall2-tech <kamalmarshall2@gmail.com>
2026-04-15 06:10:58 -0400

    Delete menu.js

--- menu.js
+++
@@ -1,7 +0,0 @@
-const menuToggle = document.getElementById('menu-toggle');
-const menu = document.getElementById('menu');
-
-menuToggle.addEventListener('click', () => {
-    menu.classList.toggle('show');
-    menuToggle.classList.toggle('active');
-});
